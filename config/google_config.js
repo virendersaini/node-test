440e3d398842fd29b55c54e3a8b752b8df3da85e
Virender <virender.saini@planetwebsolution.com>
2018-09-18 10:50:04 +0530

    asdfa

--- config/google_config.js
+++
@@ -1,9 +0,0 @@
- var options = {
-        provider: 'google',
-       
-        // Optional depending on the providers
-        httpAdapter: 'https', // Default
-        apiKey: 'AIzaSyAlNp9k6nA5z03BiEYi9djp3yZpMg5asVk', // for Mapquest, OpenCage, Google Premier
-        formatter: null         // 'gpx', 'string', ...
-      };
-       
